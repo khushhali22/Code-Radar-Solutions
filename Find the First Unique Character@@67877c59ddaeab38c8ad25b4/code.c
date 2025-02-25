2b664feafee90ca1bb18f155aca72cb7b9fe08cd
khushhali22 <khushhali1193.becse24@chitkara.edu.in>
2025-02-25 09:49:48 +0530

    Solution to Find the First Unique Character

--- Find the First Unique Character@@67877c59ddaeab38c8ad25b4/code.c
+++ Find the First Unique Character@@67877c59ddaeab38c8ad25b4/code.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
+#include<string.h>
 int main(){
     char x[100];
     scanf("%[^\n]c",x);
-    for(int i=0 ; i<n-1 ; i++){
-        for(int j=i+1 ; j<n ; j++){
+    int l=strlen(x)
+    for(int i=0 ; i<l-1 ; i++){
+        for(int j=i+1 ; j<l ; j++){
             if(x[i]!=x[j]);
             // printf("%c",x[i]);
         }
