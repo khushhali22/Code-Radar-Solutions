57e2a1f6e65c8b1b949f7f8725969cfa7a4e5cb1
khushhali22 <khushhali1193.becse24@chitkara.edu.in>
2025-03-01 13:44:07 +0530

    Solution to Calculate Employee Bonus

---
+++ Calculate Employee Bonus@@67877d61dc107486ad5c274f/code.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    struct Employee{
+        int id;
+        char name[100];
+        int salary;
+    };
+    int n;
+    scanf("%d",&n);
+    struct Employee arr[n];
+    float bonous;
+    for(int i=0; i<n ; i++){
+        scanf("%d"&arr[i].id);
+        scanf("%s"&arr[i].name);
+        scanf("%d"&arr[i].salary);
+    }
+    for(int i=0 ; i<n ; i++){
+        if(arr[i].salary<50000){
+            bonous=arr[i]*0.10;
+        }
+        else{
+             bonous=arr[i]*0.5;
+        }
+        printf("Employee ID: %d, Name: %s, Bonus: %.2f",i,arr[i].id,arr[i].name,bonous);
+    }
+    return 0;
+}
