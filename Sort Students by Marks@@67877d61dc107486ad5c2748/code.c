110371c7651c8eec957cd0a69b775cf332984d5a
khushhali22 <khushhali1193.becse24@chitkara.edu.in>
2025-03-01 12:20:40 +0530

    Solution to Sort Students by Marks

--- Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
+++ Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
@@ -14,7 +14,7 @@ int main(){
         scanf("%f",&arr[i].marks);
     }
     for(int i=0; i<n-1 ; i++){
-        for(int j=0 ; j<n-i-1 ; j++){
+        for(int j=i+1 ; j<n-i-1 ; j++){
         if(arr[j].marks>arr[i].marks){
             float temp=arr[i].marks;
             arr[i].marks=arr[j].marks;
