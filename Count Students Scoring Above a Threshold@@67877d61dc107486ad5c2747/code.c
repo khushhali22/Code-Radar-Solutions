cb855705b8c15989f6531450096cad7d44c63f43
khushhali22 <khushhali1193.becse24@chitkara.edu.in>
2025-03-01 12:00:20 +0530

    Solution to Count Students Scoring Above a Threshold

--- Count Students Scoring Above a Threshold@@67877d61dc107486ad5c2747/code.c
+++ Count Students Scoring Above a Threshold@@67877d61dc107486ad5c2747/code.c
@@ -17,7 +17,7 @@ int main(){
     scanf("%f",&threshold);
     int c=0;
     for(int i=0 ; i<n  ;i++){
-        if(threshold==arr[i].marks){
+        if(threshold<arr[i].marks){
             c++;
         }
     }
