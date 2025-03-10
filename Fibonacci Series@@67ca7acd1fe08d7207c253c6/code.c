b75e7eb58e8c99b6a7bedbeeda2e5233a8a15d66
khushhali22 <khushhali1193.becse24@chitkara.edu.in>
2025-03-10 13:07:07 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,19 @@
+void fibonaccciSeries(int n){
+    if(n==1){
+        printf("%d ",0);
+    }
+    if(n==2)[
+        printf("%d %d ",0,1);
+    ]
+    if(n>2){
+    int a=0;
+    int b=1;
+    printf("%d %d ",0,1);
+    for(int i=2 ; i<n ; i++){
+        c=a+b;
+        printf("%d ",c);
+        a=b;
+        b=c;
+    }
+    }
+}
