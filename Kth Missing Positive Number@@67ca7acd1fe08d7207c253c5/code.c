1b5fc23a78a1cd62760064fde8aa5b819259171f
khushhali22 <khushhali1193.becse24@chitkara.edu.in>
2025-03-11 10:10:11 +0530

    Solution to Kth Missing Positive Number

--- Kth Missing Positive Number@@67ca7acd1fe08d7207c253c5/code.c
+++ Kth Missing Positive Number@@67ca7acd1fe08d7207c253c5/code.c
@@ -1,12 +1,13 @@
 int findKthMissing(int arr[],int n, int k){
     int ans;
     for (int i=0 ; i<n ; i++){
-       arr[i]<k;
+       if(arr[i]<k){
        k++;
     }
     else{
         ans=k;
         break;
+    }
     }
     return ans;
 }
